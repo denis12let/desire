4921410f90e0534cec18817cc1f3347b36bb5d97
denis12let <84den84sokol@gmail.com>
2024-05-03 16:57:17 +0300

    header+footer(not adaptive)

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
-
-})
+  $('.header__btn').on('click', function () {
+    $('.right-menu').removeClass('right-menu--close');
+  });
+  $('.right-menu__close').on('click', function () {
+    $('.right-menu').addClass('right-menu--close');
+  });
+});
